e36f7b5fa00d2f43ae5c9ac084748221582aa7fc
bmalkes <bmalkes@gmail.com>
2024-04-10 16:32:56 +0300

    feat:add loader

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import AOS from "aos";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "aos/dist/aos.css";
+
 import Home from "./pages/Home";
-
 import Layout from "./layout";
-
 import NotFound from "./pages/404";
-
 import Contact from "./pages/Contact";
 import AboutPage from "./pages/About";
 import Projects from "./pages/Projects";
@@ -17,49 +15,35 @@ import DigitalPage from "./pages/DigitalPage";
 import GraphicPage from "./pages/GraphicPage";
 import DevelopmentPage from "./pages/DevelopmentPage";
 import ServerPage from "./pages/ServerPage";
-import { useEffect, useState } from "react";
-import Loader from "./components/loader";
 import Posts from "./pages/Posts";
 import Post from "./pages/Posts/post";
 
 AOS.init();
 
 function App() {
-  const [loading, setLoading] = useState(true);
   const { i18n } = useTranslation();
   document.body.dir = i18n.dir();
   const location = useLocation();
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
   return (
     <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Layout />}>
-              <Route path="/" element={<Home />} />
-              <Route path="*" element={<NotFound />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/posts" element={<Posts />} />
-              <Route path="/posts/:id" element={<Post />} />
-              <Route path="/projects" element={<Projects />} />
-              <Route path="/services" element={<Services />} />
-              <Route path="/digital" element={<DigitalPage />} />
-              <Route path="/development" element={<DevelopmentPage />} />
-              <Route path="/design" element={<GraphicPage />} />
-              <Route path="/server" element={<ServerPage />} />
-            </Route>
-          </Routes>
-        </AnimatePresence>
-      )}
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/posts/:id" element={<Post />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/digital" element={<DigitalPage />} />
+            <Route path="/development" element={<DevelopmentPage />} />
+            <Route path="/design" element={<GraphicPage />} />
+            <Route path="/server" element={<ServerPage />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
