e393c07042e61e52be5b252a4030cb5cab7c34bd
bmalkes <bmalkes@gmail.com>
2024-09-15 18:39:22 +0300

    fix:add few changes

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-// import { useEffect, useState } from "react";
-import { Tilt } from "react-tilt";
 
 const Hero = () => {
   const [direction, setDirection] = useState(document.body.dir);
