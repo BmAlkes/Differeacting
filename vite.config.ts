83829ec3c167548225e97df7edd45c7e26d3986e
bmalkes <bmalkes@gmail.com>
2024-10-03 18:52:56 +0300

    feat:add new pictures

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import generateSitemap from 'vite-plugin-pages-sitemap'
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    Pages({
+      onRoutesGenerated: routes => (generateSitemap({ routes })),
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
