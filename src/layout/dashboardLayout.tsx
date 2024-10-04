2c590503295ce6ab77af1e81d4c62c604d955161
bmalkes <bmalkes@gmail.com>
2024-10-04 15:51:40 +0300

    feat:add blog page

--- src/layout/dashboardLayout.tsx
+++ src/layout/dashboardLayout.tsx
@@ -17,7 +17,7 @@ export const DashboardLayout = () => {
     <>
         <section className="w-full  bg-gray-200 h-screen bg-[url('https://res.cloudinary.com/landingpage2/image/upload/v1715794377/Icon_C_w5j74y.png')] bg-no-repeat bg-center relative">
         <Sidebar/>
-        <HeaderApp name={data.name} profileImage={data.profileImage.filePath} />
+        <HeaderApp name={data.name} profileImage={data.profileImage?.filePath} />
         <div className={`${open ?"pl-[250px]" :"pl-[60px]" }  pr-[20px] pt-[70px]  w-full h-full overflow-y-auto`}>
           <Outlet/>
         </div>
