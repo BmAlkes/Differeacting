3c3767d5772c1cc0884db766fd50f5013bf7f998
bmalkes <bmalkes@gmail.com>
2024-10-17 18:11:03 +0300

    feat:Add option to create h6 on blog

--- src/pages/Blog/createBlog/index.tsx
+++ src/pages/Blog/createBlog/index.tsx
@@ -50,7 +50,7 @@ const CreatePost = () => {
 
   const modules = {
     toolbar: [
-      [{ header: [1, 2, 3, 4, 5, false] }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
       ["bold", "italic", "underline", "strike", "blockquote"],
       [
         { list: "ordered" },
