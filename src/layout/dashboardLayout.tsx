7ff4e907eb2f5612372377635ab9dc7a222058d2
bmalkes <bmalkes@gmail.com>
2024-11-25 20:26:43 +0200

    feat:dotvizion changes

--- src/layout/dashboardLayout.tsx
+++ src/layout/dashboardLayout.tsx
@@ -18,7 +18,7 @@ document.body.dir = "ltr"
   if(data) return (
 
     <>
-        <section className="w-full  bg-gray-200 h-screen bg-[url('https://res.cloudinary.com/landingpage2/image/upload/v1732558718/5000x5000-4_1_j6ngaz.jpg')] bg-no-repeat bg-center  relative">
+        <section className="w-full  bg-gray-200 h-screen bg-[url('https://res.cloudinary.com/landingpage2/image/upload/v1732559149/5000x5000-4__1_-removebg-preview_x6cvix.png')] bg-no-repeat bg-center  relative">
         <Sidebar/>
         <HeaderApp name={data.name} profileImage={data?.profileImage?.filePath} />
         <div className={`${open ?"pl-[250px]" :"pl-[60px]" }  pr-[20px] pt-[70px]  w-full h-full overflow-y-auto`}>
